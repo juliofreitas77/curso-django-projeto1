6df05d8558cfd64d747b7a35d1d883327ea077d3
Julio Cesar S de Freitas <julio.si.freitas@gmail.com>
2024-02-16 21:09:40 -0300

    Separando uma partial para a head

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Júlio César',
+    })
 
