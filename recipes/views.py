8947354c19379ef5958a6f07fd2ccc3d90a65c72
Julio Cesar S de Freitas <julio.si.freitas@gmail.com>
2024-02-17 20:27:08 -0300

    Uma nova URL para uma nova View

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-
-# Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Júlio César',
     })
 
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Júlio César',
+    })
